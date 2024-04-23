792d7835f019ba116c917534f3c0c4677fbb8f76
Talha Sattar <160661345+talhasattar06@users.noreply.github.com>
2024-04-23 23:22:24 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// const randomNumber: number = 11;
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answers = await inquirer.prompt([
+    {
+    name: "userNumber",
+    type: "number",
+    message: "Please guess a number between 1-5: ",
+    },
+]);
+
+if (answers.userNumber === randomNumber) {
+    console.log(" YAY! You guessed the number correctly!");
+    
+} else {
+    console.log("You did not guessed the number, Please try again!");
+    
+}
